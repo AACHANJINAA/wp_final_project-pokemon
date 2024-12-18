66d264f720f785294b8aa921d6c32b349659013a
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-12-18 14:12:13 +0900

    2022-2-1 기말과제(포켓몬모작)

---
+++ 윈도우 프로그래밍 02반 9팀 기말과제 최종/포켓몬스터 기말과제/포켓몬스터 기말과제/resource.h
@@ -0,0 +1,91 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// 포켓몬스터 기말과제.rc에서 사용되고 있습니다.
+//
+#define IDB_BITMAP1                     101
+#define IDB_BITMAP2                     102
+#define IDB_BITMAP3                     103
+#define IDB_BITMAP4                     104
+#define IDB_BITMAP5                     105
+#define IDB_BITMAP6                     106
+#define IDB_BITMAP7                     107
+#define IDB_BITMAP8                     108
+#define IDB_BITMAP9                     109
+#define IDB_BITMAP10                    110
+#define IDB_BITMAP11                    111
+#define IDB_BITMAP12                    112
+#define IDB_BITMAP13                    113
+#define IDB_BITMAP14                    114
+#define IDB_BITMAP15                    115
+#define IDB_BITMAP16                    116
+#define IDB_BITMAP17                    117
+#define IDB_BITMAP18                    118
+#define IDB_BITMAP19                    119
+#define IDB_BITMAP20                    120
+#define IDB_BITMAP21                    121
+#define IDB_BITMAP22                    122
+#define IDB_BITMAP23                    123
+#define IDB_BITMAP24                    124
+#define IDB_BITMAP25                    125
+#define IDB_BITMAP26                    126
+#define IDB_BITMAP27                    127
+#define IDB_F                           135
+#define IDB_B                           136
+#define IDB_G                           137
+#define IDB_R                           138
+#define IDB_W                           139
+#define IDB_P                           140
+#define IDB_PSKILL1                     141
+#define IDB_PSKILL2                     142
+#define IDB_FSKILL2                     144
+#define IDB_FSKILL1                     145
+#define IDB_FSKILL12                    146
+#define IDB_BSKILL1                     147
+#define IDB_WSKILL12                    148
+#define IDB_WSKILL1                     149
+#define IDB_WSKILL2                     150
+#define IDB_RSKILL1                     151
+#define IDB_RSKILL2                     152
+#define IDB_GSKILL1                     153
+#define IDB_GSKILL2                     154
+#define IDB_BSKILL2                     155
+#define IDB_0BACKGROUND                 163
+#define IDB_BSKILL22                    176
+#define IDB_M1                          181
+#define IDB_M2                          182
+#define IDB_M3                          183
+#define IDB_M3SKILL                     184
+#define IDB_BITMAP28                    185
+#define IDB_BITMAP29                    186
+#define IDB_BITMAP30                    187
+#define IDB_BITMAP31                    188
+#define IDB_BALL                        189
+#define IDB_STEAL_BUTTON                190
+#define IDB_NORMAL_BUTTON               191
+#define IDB_POISON_BUTTON               192
+#define IDB_DRAGON_BUTTON               193
+#define IDB_GROUND_BUTTON               194
+#define IDB_WATER_BUTTON                195
+#define IDB_ROCK_BUTTON                 196
+#define IDB_FIRE_BUTTON                 197
+#define IDB_ELECTRO_BUTTON              198
+#define IDB_GRASS_BUTTON                199
+#define IDB_BITMAP32                    200
+#define IDB_BITMAP33                    201
+#define IDB_BITMAP34                    203
+#define IDB_BITMAP35                    204
+#define IDB_BITMAP36                    205
+#define IDB_BITMAP37                    206
+#define IDB_BITMAP38                    208
+#define IDB_BITMAP39                    209
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        210
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
